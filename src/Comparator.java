2ab9a9f2ec906b63ec75b1297065823c94ac0128
ArnasJonynas <37830593+ArnasJonynas@users.noreply.github.com>
2018-04-30 12:26:30 +0300

    Update Comparator.java

--- src/Comparator.java
+++ src/Comparator.java
@@ -17,6 +17,7 @@ public class Comparator {
     Eventduom enduo = new Eventduom();
     Dateconverter dacon = new Dateconverter();
     Time data = new Time(); 
+   
     
     public ArrayList<Eventduom> neededEvents = new ArrayList<Eventduom>();
     String error=("There is no this tipe events for your selected day");
@@ -41,7 +42,7 @@ public class Comparator {
                  System.out.println("\n"+ eData);
                  System.out.println("\n");
                 if(theday.before(eData)){           // palyginam data ar ideti ar ne
-                           if (age>enduo.getAge()) {
+                           if (age>sarasas.get(i).getAge()){
                                  neededEvents.add(sarasas.get(i));
                                  
                        //System.out.println(neededEvents);
@@ -49,13 +50,20 @@ public class Comparator {
                     }
                 }
                 
-            }  // Issirenkam pavadinimus renginiu 
+            } 
+
+        // if were is no events for selected day
         if (neededEvents.size()==0){
-            System.out.println(error);
-           // tipes(sarasas,age,theday);
-            
-        }
-          String [] names = new String [neededEvents.size()];     
+           int erorr=gui.ErrosMessage();
+            if (erorr == 0){
+                Date day = data.addDay(1);
+                tipes(sarasas,age,day);
+            }
+       
+        } 
+        // ---------------------------------
+        else {
+            String [] names = new String [neededEvents.size()];     
              System.out.println(neededEvents);
           for(int i = 0; i < neededEvents.size(); i++){
               names[i] = neededEvents.get(i).getEvent();
@@ -70,26 +78,51 @@ public class Comparator {
               int place = neededEvents.get(name_index).getPlace();
               if (place==0)
               {
-                  place++;
-                  gui.Entername();
-                  
+                   int registed = neededEvents.get(name_index).getRegistered();
+                   registed++;
+                   int maxregisted =neededEvents.get(name_index).getHowmanyalowd();
+                  String rate1 = Rating(registed, maxregisted);
+                   gui.Entername(rate1);
               }
-              else {
+              else if(place==1) {
                  int howmanyregistered = neededEvents.get(name_index).getHowmanyalowd();
                  int registered = neededEvents.get(name_index).getRegistered();
                     if (registered<howmanyregistered){
-                        gui.Entername();
+                    int registed = neededEvents.get(name_index).getRegistered();
+                    registed++;
+                   int maxregisted =neededEvents.get(name_index).getHowmanyalowd();
+                    String rate1 = Rating(registed, maxregisted);
+                       String rate2 = Rating(registed, maxregisted);
+                        gui.Entername(rate2);
                     }
-                    else gui.Eventnamesprint(names);
+                    //else gui.Eventnamesprint(names);
               }
           }
-          else { 
+          else if(regist_index == 1) { 
               tipes(sarasas,age,theday);
           }
-         
+        
           return names;
+        }
+        return null;
+        
     }
     
-     
+    public String Rating ( int min, int max){
+            
+                  String rating = null ;  // will save the message about event rating
+                  // calculating rating
+                  double proc;
+                  proc = ( min/max );     // event rating = how many registered/how many max could registed  tell the rating
+                  if (proc > 0.75){
+                       rating = "The event is sucesful, many people have registered" ;
+                  }
+                  else if (0.5<= proc && proc < 0.75){
+                       rating = "The event is fine :) " ; 
+                  }
+                  else if ( proc < 0.5){
+                      rating = " The event is unsusesful, we hope more people will regist to event";
+              }
+         return rating;
+     }
 }
-
