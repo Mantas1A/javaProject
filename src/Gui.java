014e1c0491fbce764715e166406346f81e00d91a
Mantas1A <Mantasas15@gmail.com>
2018-04-30 12:39:03 +0300

    Update Gui.java

--- src/Gui.java
+++ src/Gui.java
@@ -1,60 +1,68 @@
+package src;
 
-import java.util.ArrayList;
+import src.Eventduom;
+
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ruceg
- */
 public class Gui {
     
     public int age (){
-    
-    String fn = JOptionPane.showInputDialog("Enter your age");
-    int age = Integer.parseInt(fn);
-    // JOptionPane.showMessageDialog(null, "your age" + age, "the title ", JOptionPane.PLAIN_MESSAGE);
-    return age;
-                      }
+        String fn = "";
+        fn = JOptionPane.showInputDialog("Enter your age: ");
+        while(fn.equals("")){
+            fn = JOptionPane.showInputDialog("Enter your age: ");
+        }
+        try {
+            int age = Integer.parseInt(fn);
+        }catch(Exception e){
+
+            JPanel panel = new JPanel();
+            JOptionPane.showMessageDialog(panel,"Enter a number!");
+            System.exit(0);
+        }
+        int age = Integer.parseInt(fn);
+        // JOptionPane.showMessageDialog(null, "your age" + age, "the title ", JOptionPane.PLAIN_MESSAGE);
+        return age;
+    }
     
     public String date(){
-    String strDate = JOptionPane.showInputDialog("Enter the date   Format:yyyy/MM/dd");
+        String strDate = JOptionPane.showInputDialog("Enter the date:   Format:yyyy/MM/dd");
     
-    return strDate;
+        return strDate;
     }
     
     public int Eventstipes(){
+        String[] events = {"Sports", "Conferences", "Concerts", "Seminars", "Educational"};
+        int event_index = JOptionPane.showOptionDialog(null,"To which type of the event would You like to go?", "Tipes of the Events", 0, JOptionPane.QUESTION_MESSAGE,null,events,0 );
        
-        
-        String[] events = {"Sports", "Conferences", "Trade Shows", "Seminars", "Team Building Events"};
-      int event_index = JOptionPane.showOptionDialog(null,"In what event you would like to go?", "Events Tipe", 0, JOptionPane.QUESTION_MESSAGE,null,events,0 );
+        // System.out.println(date_index);
        
-       // System.out.println(date_index);
-       
-       return event_index;
+        return event_index;
     
     }
-      public int Eventnamesprint(String[] names){
-            int name_index = JOptionPane.showOptionDialog(null,"In what event you would like to go?", "Events Names", 0, JOptionPane.QUESTION_MESSAGE,null,names,0 );
-         return name_index;
-      }
-      
-      public int Eventinfo( Eventduom sarasas ){
-          String [] info = new String [] {"Regist", "Back to Events"}; 
-          int info_index = JOptionPane.showOptionDialog(null,sarasas, "Event", 0, JOptionPane.QUESTION_MESSAGE,null,info,0 );
-         return info_index;
-      }
-      public void Entername()
-      {
-          String name = JOptionPane.showInputDialog("Enter your name ");
-          String surname = JOptionPane.showInputDialog("Enter your surname");
-          JOptionPane.showMessageDialog(null, "You have been registed "+name+ " "+surname, "The title", JOptionPane.PLAIN_MESSAGE);
-        
-          
-      }
+    
+    public int Eventnamesprint(String[] names){
+        int name_index = JOptionPane.showOptionDialog(null,"To which event would You like to go?", "Names of the Events", 0, JOptionPane.QUESTION_MESSAGE,null,names,0 );
+        return name_index;
     }
+      
+    public int Eventinfo(Eventduom sarasas ){
+        String [] info = new String [] {"Register", "Back to Events"}; 
+        int info_index = JOptionPane.showOptionDialog(null, sarasas, "Event", 0, JOptionPane.QUESTION_MESSAGE,null,info,0 );
+        return info_index;
+    }
+    
+    public void Entername(){
+        String name;
+        String surname;
+        do {
+            name = JOptionPane.showInputDialog("Enter your name: ");
+
+        } while (name.equals(""));
+        do {
+            surname = JOptionPane.showInputDialog("Enter your surname: ");
+        }while (surname.equals(""));
+        JOptionPane.showMessageDialog(null, "You have been registed: "+name+ " "+surname, "The title", JOptionPane.PLAIN_MESSAGE);
+    }
+}
